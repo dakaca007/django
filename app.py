ecc9a241e5f9c6f91a185e0c059e4694991a2c67
dakaca007 <129284180+dakaca007@users.noreply.github.com>
2023-11-10 14:48:35 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,9 @@
-print(1234)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, 1234!'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80)
