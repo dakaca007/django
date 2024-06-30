783792b27d252a8afc0bac82b3c0d7482122ff46
dakaca007 <129284180+dakaca007@users.noreply.github.com>
2024-06-30 17:39:43 +0800

    Update app.py
    890

--- app.py
+++ app.py
@@ -11,6 +11,9 @@ import os
 from werkzeug.utils import secure_filename
 import sys
 import pymysql 
+import socket
+
+
 app = Flask(__name__, static_folder='static', static_url_path='/static')
 # MySQL数据库配置
 app.config['MYSQL_HOST'] = 'mysql.sqlpub.com:3306'
@@ -18,7 +21,37 @@ app.config['MYSQL_USER'] = 'dakaca007'
 app.config['MYSQL_PASSWORD'] = 'Kgds63EecpSlAtYR'
 app.config['MYSQL_DB'] = 'dakaca'
 
+def test_mysql_connection(host, port):
+    try:
+        # 创建 socket 连接
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        # 设置连接超时时间为 5 秒
+        sock.settimeout(5)
+        
+        # 连接 MySQL 主机的指定端口
+        result = sock.connect_ex((host, port))
+        
+        if result == 0:
+            # 连接成功
+            return True
+        else:
+            # 连接失败
+            return False
+    except socket.error:
+        # 发生异常，连接失败
+        return False
+    finally:
+        # 关闭 socket 连接
+        sock.close()
 
+# 测试连接
+host = 'mysql.sqlpub.com'
+port = 3306
+
+if test_mysql_connection(host, port):
+    print("MySQL数据库连接成功！")
+else:
+    print("MySQL数据库连接失败！")
 @app.route("/")
 def index():
     return render_template("index.html")
