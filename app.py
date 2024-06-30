0d972a443914a303445140691f77b923a63bbe51
dakaca007 <129284180+dakaca007@users.noreply.github.com>
2024-06-30 15:49:17 +0800

    Update app.py
    f

--- app.py
+++ app.py
@@ -15,7 +15,9 @@ app = Flask(__name__, static_folder='static', static_url_path='/static')
 @app.route("/")
 def index():
     return render_template("index.html")
-
+@app.route("/test")
+def test():
+    return render_template("test.html")
 
  
 
