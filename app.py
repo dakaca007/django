7a9626396454b176422904893ac9291446ef2ffe
dakaca007 <129284180+dakaca007@users.noreply.github.com>
2024-06-30 16:34:56 +0800

    Update app.py
    666

--- app.py
+++ app.py
@@ -10,15 +10,27 @@ import json
 import os
 from werkzeug.utils import secure_filename
 import sys
+import mysql.connector
 app = Flask(__name__, static_folder='static', static_url_path='/static')
-         
+cnx = mysql.connector.connect(
+    user='dakaca007',
+    password='Kgds63EecpSlAtYR',
+    host='mysql.sqlpub.com:3306',
+    database='dakaca'
+)       
 @app.route("/")
 def index():
     return render_template("index.html")
 @app.route("/test")
 def test():
     return render_template("test.html")
-
+@app.route('/query')
+def query_data():
+    cursor = cnx.cursor()
+    cursor.execute("SELECT * FROM user")
+    data = cursor.fetchall()
+    cursor.close()
+    return render_template('result.html', data=data)
  
 
 if __name__ == '__main__':
