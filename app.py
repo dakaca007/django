ebdf7e72edecc128b136ceddd664d66c3ed9fcac
dakaca007 <129284180+dakaca007@users.noreply.github.com>
2024-06-30 18:08:50 +0800

    Update app.py
    11

--- app.py
+++ app.py
@@ -54,8 +54,120 @@ def test():
     cursor.close()
     conn.close()
     return {'data': result}
- 
+@app.route("/add_user", methods=["POST"])
+def add_user():
+    # 获取POST请求中的数据
+    first_name = request.form.get("first_name")
+    last_name = request.form.get("last_name")
+    email = request.form.get("email")
+
+    # 创建数据库连接
+    conn = pymysql.connect(
+        host=app.config['MYSQL_HOST'],
+        user=app.config['MYSQL_USER'],
+        password=app.config['MYSQL_PASSWORD'],
+        db=app.config['MYSQL_DB']
+    )
+    
+    # 创建游标
+    cursor = conn.cursor()
+
+    # 执行插入操作
+    cursor.execute("INSERT INTO user (first_name, last_name, email) VALUES (%s, %s, %s)", (first_name, last_name, email))
+
+    # 提交事务
+    conn.commit()
+
+    # 关闭游标和数据库连接
+    cursor.close()
+    conn.close()
+
+    return "User added successfully"
+@app.route("/delete_user/<int:user_id>", methods=["GET"])
+def delete_user(user_id):
+    # 创建数据库连接
+    conn = pymysql.connect(
+        host=app.config['MYSQL_HOST'],
+        user=app.config['MYSQL_USER'],
+        password=app.config['MYSQL_PASSWORD'],
+        db=app.config['MYSQL_DB']
+    )
+    
+    # 创建游标
+    cursor = conn.cursor()
+
+    # 执行删除操作
+    cursor.execute("DELETE FROM user WHERE id = %s", (user_id,))
+
+    # 提交事务
+    conn.commit()
+
+    # 关闭游标和数据库连接
+    cursor.close()
+    conn.close()
+
+    return "User deleted successfully"
  
 
+@app.route("/update_user/<int:user_id>", methods=["POST"])
+def update_user(user_id):
+    # 获取POST请求中的数据
+    first_name = request.form.get("first_name")
+    last_name = request.form.get("last_name")
+    email = request.form.get("email")
+
+    # 创建数据库连接
+    conn = pymysql.connect(
+        host=app.config['MYSQL_HOST'],
+        user=app.config['MYSQL_USER'],
+        password=app.config['MYSQL_PASSWORD'],
+        db=app.config['MYSQL_DB']
+    )
+    
+    # 创建游标
+    cursor = conn.cursor()
+
+    # 执行更新操作
+    cursor.execute("UPDATE user SET first_name = %s, last_name = %s, email = %s WHERE id = %s", (first_name, last_name, email, user_id))
+
+    # 提交事务
+    conn.commit()
+
+    # 关闭游标和数据库连接
+    cursor.close()
+    conn.close()
+
+    return "User updated successfully"
+@app.route("/get_user/<int:user_id>", methods=["GET"])
+def get_user(user_id):
+    # 创建数据库连接
+    conn = pymysql.connect(
+        host=app.config['MYSQL_HOST'],
+        user=app.config['MYSQL_USER'],
+        password=app.config['MYSQL_PASSWORD'],
+        db=app.config['MYSQL_DB']
+    )
+    
+    # 创建游标
+    cursor = conn.cursor()
+
+    # 执行查询操作
+    cursor.execute("SELECT * FROM user WHERE id = %s", (user_id,))
+    data = cursor.fetchone()
+
+    # 关闭游标和数据库连接
+    cursor.close()
+    conn.close()
+
+    if data:
+        result = {
+            'id': data[0],
+            'first_name': data[1],
+            'last_name': data[2],
+            'email': data[3]
+        }
+        return jsonify(result)
+    else:
+        return "User not found"
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
