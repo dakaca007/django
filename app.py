7adae842daf7cd5dbb5697980a2238fc5a557b58
dakaca007 <129284180+dakaca007@users.noreply.github.com>
2024-07-02 09:25:06 +0800

    Update app.py
    999

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ def index():
     # 执行 PHP 脚本
     process = subprocess.check_output(['php', 'index.php'])
     # 获取 PHP 脚本输出的 HTML 内容
-    html_content = process.stdout.decode('utf-8')
+    html_content = process.decode('utf-8')
     # 在 Flask 模板中渲染 HTML 内容
     return render_template('index.html', html_content=html_content)
 @app.route("/user_list")
