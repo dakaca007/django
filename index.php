151232b512247c733ffdfb967055b9fd5530905c
dakaca007 <129284180+dakaca007@users.noreply.github.com>
2024-07-02 09:56:56 +0800

    Update index.php
    sdfsxd

--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 <?php
-$user_name = isset($_GET['user_name']) ? $_GET['user_name'] : '';
-$product_id = isset($_GET['product_id']) ? $_GET['product_id'] : '';
-// 使用 $user_name 和 $product_id 来执行您需要的任务
-echo $user_name;
-echo $product_id;
+// 获取参数
+$user_name = $_SERVER['argv'][1]; // 提取第一个参数
+$product_id = $_SERVER['argv'][2]; // 提取第二个参数
+
+// 使用参数进行操作
+echo "User Name: $user_name";
+echo "<br>";
+echo "Product ID: $product_id";
