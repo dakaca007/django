217c5731a3cb6f1fcf2551fda4cd9683140faa1b
dakaca007 <129284180+dakaca007@users.noreply.github.com>
2024-07-02 09:50:27 +0800

    Update index.php
    sdfsdf

--- index.php
+++ index.php
@@ -3,4 +3,4 @@ $user_name = isset($_GET['user_name']) ? $_GET['user_name'] : '';
 $product_id = isset($_GET['product_id']) ? $_GET['product_id'] : '';
 // 使用 $user_name 和 $product_id 来执行您需要的任务
 echo $user_name;
-echo $product_id
+echo $product_id;
