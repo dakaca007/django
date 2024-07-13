b40ca2a4f407e2c111d9514818ff916c5c8e8fb2
dakaca007 <129284180+dakaca007@users.noreply.github.com>
2024-07-13 15:50:31 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,30 @@
 <?php
-// 获取参数
-$user_name = $_SERVER['argv'][1]; // 提取第一个参数
-$product_id = $_SERVER['argv'][2]; // 提取第二个参数
+$dsn = 'mysql:host=mysql.sqlpub.com;dbname=dakaca';
+$user = 'dakaca007';
+$password = 'Kgds63EecpSlAtYR';
 
-// 使用参数进行操作
-echo "User Name: $user_name";
-echo "<br>";
-echo "Product ID: $product_id";
+try {
+    $pdo = new PDO($dsn, $user, $password, [
+        PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8mb4"
+    ]);
+    // 设置PDO错误模式为异常
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
+    // 查询user表的数据
+    $sql = "SELECT * FROM user";
+    $stmt = $pdo->query($sql);
+
+    // 输出数据
+    $users = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    if (count($users) > 0) {
+        foreach ($users as $user) {
+            echo "ID: " . $user["id"]. " - Username: " . $user["first_name"]. " - 
+ Lastname: " . $user["last_name"]. " - Email: " . $user["email"]. "<br>";
+        }
+    } else {
+        echo "user表中没有数据";
+    }
+} catch (PDOException $e) {
+    echo "连接失败: " . $e->getMessage();
+}
+?>
