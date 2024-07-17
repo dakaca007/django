0dd4245f94f97b384bfb5958203e630bc9097c72
dakaca007 <129284180+dakaca007@users.noreply.github.com>
2024-07-17 15:15:21 +0800

    Update forum_backend.php

--- forum_backend.php
+++ forum_backend.php
@@ -1,9 +1,9 @@
 <?php
 function openDatabaseConnection() {
-    $dbHost = 'localhost';
-    $dbName = 'your_database_name';
-    $dbUsername = 'username';
-    $dbPassword = 'password';
+    $dbHost = 'mysql.sqlpub.com';
+    $dbName = 'dakacan';
+    $dbUsername = 'dakacan007';
+    $dbPassword = '4EQ9JZDnPSg36FXy';
 
     return new PDO("mysql:host=$dbHost;dbname=$dbName", $dbUsername, $dbPassword);
 }
