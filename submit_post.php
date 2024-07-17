53b48bd2c38d497f9098d5e6b959914c84bdcd39
dakaca007 <129284180+dakaca007@users.noreply.github.com>
2024-07-17 18:14:03 +0800

    Update submit_post.php

--- submit_post.php
+++ submit_post.php
@@ -1,12 +1,12 @@
 <?php
 session_start();
 require_once 'database.php';
-
+$userId = $_SESSION['UserID'];
 // 获取用户提交的标题和内容
 $title = $_POST['title'];
 $content = $_POST['content'];
 $fenlei=$_POST['fenlei']
-$userId = $_SESSION['UserID'];
+
 
 // 插入到数据库中
 $pdo = openDatabaseConnection();
@@ -16,7 +16,7 @@ $stmt->execute([$title, $content, $userId, $fenlei]);
 $pdo = null; 
 // 可以设置一个成功发布的提示信息
 $_SESSION['message'] = "成功发布新帖子！";
-$_SESSION['UserID']=$userId ;
+
 // 重定向到其他页面，比如首页或者新发表的帖子页面
 header("Location: post.php");
 exit();
