ff5be4ae8f7f204ff3635b6666d5b9327aaad704
dakaca007 <129284180+dakaca007@users.noreply.github.com>
2025-06-16 23:09:10 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # 后台运行所有爬虫脚本
 for script in p*.py; do
-    if [[ "$script" != "app.py" ]]; then
+    if [[ "$script" == "app.py" ]]; then
         echo "启动: $script"
         python3 "$script" &
     fi
 done
-python3 app.py    # 前台启动 Flask，保持容器运行
+#python3 app.py    # 前台启动 Flask，保持容器运行
