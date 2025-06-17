160ee32c530acee130b3c7ce8a8916a6f0fdfa45
dakaca007 <129284180+dakaca007@users.noreply.github.com>
2025-06-17 12:58:18 +0000

    a

--- start.sh
+++ start.sh
@@ -3,10 +3,3 @@
 
 python3 app.py    # 前台启动 Flask，保持容器运行
 
-for script in p*.py; do
-    if [[ "$script" == "app.py" ]]; then
-        echo "启动: $script"
-        python3 "$script" &
-    fi
-done
-#python3 app.py    # 前台启动 Flask，保持容器运行
